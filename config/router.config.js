7bfd3361329535d0d3df042c9ea9e8e8fcdbe0a4
pan1.zhang@changhong.com <pan1.zhang@changhong.com>
2022-02-13 21:50:19 +0800

    优化

--- config/router.config.js
+++ config/router.config.js
@@ -11,16 +11,15 @@ export default [
   {
     path: '/',
     component: '../layouts/AuthLayout',
-    title: '帮助中心',
     routes: [
-      { _title: 'dashboard', path: '/dashboard', component: './Dashboard' },
-      { _title: 'sso', path: '/sso', component: './Sso' },
-      { _title: 'homepage', path: '/homepage', component: './HomePage' },
-      { _title: 'usercenter', path: '/usercenter', component: './UserCenter' },
+      { title: '帮助中心', path: '/dashboard', component: './Dashboard' },
+      { title: '帮助中心', path: '/sso', component: './Sso' },
+      { title: '帮助中心', path: '/homepage', component: './HomePage' },
+      { title: '帮助中心', path: '/usercenter', component: './UserCenter' },
       // { title: '话题', path: '/article', component: './Article' },
-      { _title: '贴子明细', path: '/postDetail/:id', component: './PostDetail' },
-      { _title: '创建新帖', path: '/post/create', component: './Post' },
-      { _title: '编辑新帖', path: '/post/edit/:id', component: './Post/edit' },
+      { title: '帮助中心', path: '/postDetail/:id', component: './PostDetail' },
+      { title: '帮助中心', path: '/post/create', component: './Post' },
+      { title: '帮助中心', path: '/post/edit/:id', component: './Post/edit' },
     ],
   },
 ];
